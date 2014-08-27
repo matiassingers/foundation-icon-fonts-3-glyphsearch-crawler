8b71d233cc4636fdef04ff4b950492a712b5f1ae
Matias Singers <mail@matiassingers.com>
2014-08-27 20:28:26 +0800

    Create initial foundation.js - just adapted slightly from octicons-glyphsearch-crawler

---
+++ foundation.js
@@ -0,0 +1,60 @@
+var request = require('promise-request');
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+
+var destination = process.argv.slice(2)[0];
+
+function run(){
+  fetchWebpage()
+    .then(handleItems)
+    .then(writeFile);
+}
+
+function fetchWebpage(){
+  var url = 'http://zurb.com/playground/foundation-icon-fonts-3';
+
+  return request(url)
+    .then(function(body){
+      $ = cheerio.load(body);
+
+      return $('.large-12.columns .small-block-grid-2.large-block-grid-4 li p').toArray();
+    });
+}
+
+function handleItems(items){
+  var icons = _.map(items, function(item){
+    var element = $(item);
+
+    return {
+      name: element.data('dropdown'),
+      tags: element.data('dropdown')
+    };
+  });
+
+  return {
+    foundation: icons
+  };
+}
+
+function writeFile(icons){
+  var file = destination ? path.join(destination, 'data', 'icons-foundation.json') : path.join(__dirname, 'icons-foundation.json');
+  var data = JSON.stringify(icons, null, 2);
+
+  console.log('writing file to ' + file);
+
+  fs.writeFile(file, data, function(error){
+    if(error)
+      return console.log(error);
+  });
+}
+
+if(require.main === module) {
+  run();
+}
+
+module.exports = {
+  run: run,
+  fetchWebpage: fetchWebpage
+};
